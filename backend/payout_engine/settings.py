fa86026ae1e7a57a22fca8d338e0bfe171868ad7
pratzzz2432 <142898815+pratzzz2432@users.noreply.github.com>
2026-04-28 13:10:57 +0530

    Modify CORS settings in settings.py
    fix duplicate cors origins override

--- backend/payout_engine/settings.py
+++ backend/payout_engine/settings.py
@@ -29,7 +29,6 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,9 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    "https://motion-payout-engine-playto-1.onrender.com",
-]
+
 ROOT_URLCONF = 'payout_engine.urls'
 
 TEMPLATES = [
@@ -147,11 +144,12 @@ REST_FRAMEWORK = {
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    "https://motion-payout-engine-playto-1.onrender.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Celery Configuration
 CELERY_BROKER_URL = config('REDIS_URL', default='redis://localhost:6379/0')
